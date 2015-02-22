1c97986d87dc385c0e80ab5d28effc939973ac3b
Emmanuel Alhadeff <emmanuel@dudamobile.com>
2015-02-22 16:09:17 +0200

    Added bootstrap (possibly unreliable)

--- ember/app/models/snippet.js
+++ ember/app/models/snippet.js
@@ -4,6 +4,7 @@ export default DS.Model.extend({
   text: DS.attr('string', {
   	defaultValue: 'Nothing'
   }),
+  date: DS.attr('string'),
   textChanged: function() {
   	if (this.get("isDirty") && !this.get("isNew")) {
   		// this.save();
